5f31af5f4b6be0261704bf6b6ef40f7ebc5f913b
Faizan Kazi <79440289+faizanwebdev@users.noreply.github.com>
2022-04-04 21:54:19 +0530

    Update cert-gen.php

--- cert-gen.php
+++ cert-gen.php
@@ -98,8 +98,8 @@
                     $mail->isSMTP();           
                     $mail->Host = 'smtp.gmail.com';   
                     $mail->SMTPAuth = true;     
-                    $mail->Username = 'faizan.kazi@enlyft.in';        
-                    $mail->Password = 'Securityenlyft@2022#$%';    
+                    $mail->Username = 'enable.onl@gmail.com';        
+                    $mail->Password = '';    
                     $mail->SMTPSecure = 'tls';   
                     $mail->Port = 587;
                     $mail->setFrom('membership@aicpdf.com', 'AICPDF');
@@ -206,8 +206,8 @@
                     $mail->isSMTP();           
                     $mail->Host = 'smtp.gmail.com';   
                     $mail->SMTPAuth = true;     
-                    $mail->Username = 'faizan.kazi@enlyft.in';        
-                    $mail->Password = 'Securityenlyft@2022#$%';    
+                    $mail->Username = 'enable.onl@gmail.com';        
+                    $mail->Password = '';    
                     $mail->SMTPSecure = 'tls';   
                     $mail->Port = 587;
                     $mail->setFrom('membership@aicpdf.com', 'AICPDF');
