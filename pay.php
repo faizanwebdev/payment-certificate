e49094a8e4580bf54423855a976f94f7fed0a8e1
Faizan Kazi <79440289+faizanwebdev@users.noreply.github.com>
2022-04-12 12:18:57 +0530

    Update pay.php

--- pay.php
+++ pay.php
@@ -12,7 +12,7 @@
 		
         $ch = curl_init('https://api.razorpay.com/v1/payments/'.$razorPayId.'');
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-		curl_setopt($ch, CURLOPT_USERPWD, "rzp_test_GmxjuICafMVMnA:wrqlWetUo11cuuaFltcmYR63"); // Input your Razorpay Key Id and Secret Id here
+		curl_setopt($ch, CURLOPT_USERPWD, ":"); // Input your Razorpay Key Id and Secret Id here
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$response = json_decode(curl_exec($ch));
 		
